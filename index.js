501c7caf374f56e32c83cc5c250d8087239184be
nagaraju7752 <nagaraju7752@gmail.com>
2021-06-24 20:20:39 +0530

    Implemented calling external api using https and request in node Test

---
+++ index.js
@@ -0,0 +1,39 @@
+
+const express = require('express');
+const cors = require('cors');
+const request = require('request');
+const https = require('https');
+const app = express();
+require('dotenv').config();
+app.use(cors());
+// Using request for calling external api
+app.get('/users', (req, resp) => {
+    let page = req.query.page;
+    request(`https://reqres.in/api/users?page=${page}`, async (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            return await resp.json(JSON.parse(body));
+        }
+    });
+});
+
+// Using https for calling external api
+app.get('/getusers', (req, res) => {
+    let page = req.query.page
+    https.get(`https://reqres.in/api/users?page=${page}`, (resp) => {
+        let data = '';
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        resp.on('end', () => {
+            res.json(JSON.parse(data));
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+})
+
+const port = process.env.port || 3000;
+var server = app.listen(port, () => {
+    console.log("server is listening at ", host, port)
+})
